17d8623990e87d85cccee1910b87f124fde2d75e
metalfurius <hackero1932@gmail.com>
2024-04-13 09:10:44 +0200

    Feat: Able to create board

--- Assets/Scripts/Model/D2048.cs
+++ Assets/Scripts/Model/D2048.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class D2048
 {
     public int score;
-    public D2048(int score)
+    public int[,] board;
+    public D2048(int score,Vector2Int board)
     {
         this.score = score;
+        this.board = new int[board.x,board.y];
     }
     public void MergeTiles(int mergedValue)
     {
--- Assets/Scripts/Tests/Tests2048.cs
+++ Assets/Scripts/Tests/Tests2048.cs
@@ -9,14 +9,20 @@ public class Tests2048
     [Test]
     public void TestScoreDefault()
     {
-        D2048 test=new D2048(0);
+        D2048 test = new D2048(0, new Vector2Int(1,1));
         Assert.AreEqual(0, test.score);
     }
     [Test]
     public void TestScoreAfterMerge()
     {
-        D2048 test = new D2048(0);
+        D2048 test = new D2048(0,new Vector2Int(1, 1));
         test.MergeTiles(8);
         Assert.AreEqual(8, test.score);
+    }
+    [Test]
+    public void TestCreateBoard()
+    {
+        D2048 test = new D2048(0, new Vector2Int(1, 1));
+        Assert.IsNotNull(test.board);
     }
 }
