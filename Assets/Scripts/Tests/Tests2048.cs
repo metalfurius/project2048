034daef170a0500f30aee3c43874ab88fc62708d
metalfurius <hackero1932@gmail.com>
2024-04-13 09:16:57 +0200

    Test: Generate new tile test

--- Assets/Scripts/Tests/Tests2048.cs
+++ Assets/Scripts/Tests/Tests2048.cs
@@ -25,4 +25,28 @@ public class Tests2048
         D2048 test = new D2048(0, new Vector2Int(1, 1));
         Assert.IsNotNull(test.board);
     }
+    [Test]
+    public void TestGenerateNewTile()
+    {
+        D2048 test = new D2048(0, new Vector2Int(4, 4));
+        test.GenerateNewTile();
+
+        bool newTileGenerated = false;
+        for (int i = 0; i < test.board.GetLength(0); i++)
+        {
+            for (int j = 0; j < test.board.GetLength(1); j++)
+            {
+                if (test.board[i, j] != 0)
+                {
+                    newTileGenerated = true;
+                    break;
+                }
+            }
+            if (newTileGenerated)
+            {
+                break;
+            }
+        }
+        Assert.IsTrue(newTileGenerated);
+    }
 }
