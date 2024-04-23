de12f99b05de35c1280666d5a6df3e41d9f48303
metalfurius <hackero1932@gmail.com>
2024-04-23 20:37:40 +0200

    Test: Tile sets to 0 after moving

--- Assets/Scripts/Tests/Tests2048.cs
+++ Assets/Scripts/Tests/Tests2048.cs
@@ -55,4 +55,16 @@ public class Tests2048
 
         Assert.AreEqual(4, game.board[0, 0]);
     }
+
+    public void TestMoveTileCleanup()
+    {
+        D2048 game = new D2048(0, new Vector2Int(4, 4));
+
+        game.board[0, 0] = 2;
+        game.board[1, 0] = 2;
+
+        game.MoveTiles(D2048.Up);
+
+        Assert.AreEqual(0, game.board[1, 0]);
+    }
 }
