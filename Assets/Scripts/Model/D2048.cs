54d90e2dc2acbbf2aad5f4b3a4611839b9e702a8
metalfurius <hackero1932@gmail.com>
2024-05-17 18:48:42 +0200

    Feat: Tiles can now move to the right

--- Assets/Scripts/Model/D2048.cs
+++ Assets/Scripts/Model/D2048.cs
@@ -46,7 +46,7 @@ public class D2048
                         }
                         else
                         {
-                            MoveTile(i, j, targetRow + 1); // Mueve la casilla a la posición objetivo
+                            MoveTile(i, j, targetRow + 1, j); // Mueve la casilla a la posición objetivo
                         }
                     }
                 }
@@ -68,7 +68,29 @@ public class D2048
                         }
                         else
                         {
-                            MoveTile(i, j, targetRow - 1); // Mueve la casilla a la posición objetivo
+                            MoveTile(i, j, targetRow - 1, j); // Mueve la casilla a la posición objetivo
+                        }
+                    }
+                }
+            }
+        }
+        else if (direction == Right)
+        {
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = board.GetLength(1) - 2; j >= 0; j--) // Comenzamos desde la penúltima columna
+                {
+                    if (board[i, j] != 0) // Ignora las casillas vacías
+                    {
+                        int targetColumn = FindTargetColumn(i, j, direction); // Busca la columna objetivo
+
+                        if (targetColumn < board.GetLength(1) && board[i, targetColumn] == board[i, j]) // Comprueba si hay combinación
+                        {
+                            MergeTilesAndUpdateScore(i, j, i, targetColumn);
+                        }
+                        else
+                        {
+                            MoveTile(i, j, i, targetColumn - 1); // Mueve la casilla a la posición objetivo
                         }
                     }
                 }
@@ -86,19 +108,36 @@ public class D2048
         return targetRow;
     }
 
-    private void MergeTilesAndUpdateScore(int currentRow, int column, int targetRow)
+    private int FindTargetColumn(int row, int currentColumn, Vector2Int direction)
     {
-        board[targetRow, column] *= 2;
-        board[currentRow, column] = 0;
-        MergeTiles(board[targetRow, column]); // Actualiza la puntuación
+        int targetColumn = currentColumn + direction.x;
+        while (targetColumn >= 0 && targetColumn < board.GetLength(1) && board[row, targetColumn] == 0)
+        {
+            targetColumn += direction.x;
+        }
+        return targetColumn;
     }
 
-    private void MoveTile(int currentRow, int column, int targetRow)
+    private void MergeTilesAndUpdateScore(int currentRow, int currentColumn, int targetRow)
     {
-        board[targetRow, column] = board[currentRow, column];
-        if (targetRow != currentRow)
+        board[targetRow, currentColumn] *= 2;
+        board[currentRow, currentColumn] = 0;
+        MergeTiles(board[targetRow, currentColumn]); // Actualiza la puntuación
+    }
+
+    private void MergeTilesAndUpdateScore(int currentRow, int currentColumn, int targetRow, int targetColumn)
+    {
+        board[targetRow, targetColumn] *= 2;
+        board[currentRow, currentColumn] = 0;
+        MergeTiles(board[targetRow, targetColumn]); // Actualiza la puntuación
+    }
+
+    private void MoveTile(int currentRow, int currentColumn, int targetRow, int targetColumn)
+    {
+        board[targetRow, targetColumn] = board[currentRow, currentColumn];
+        if (targetRow != currentRow || targetColumn != currentColumn)
         {
-            board[currentRow, column] = 0;
+            board[currentRow, currentColumn] = 0;
         }
     }
 }
