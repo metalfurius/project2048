130db38587b1951bb6fa7cff3882b0fac2e2320f
metalfurius <hackero1932@gmail.com>
2024-07-12 11:43:25 +0200

    refactor: board animator

--- Assets/Scripts/View/BoardAnimator.cs
+++ Assets/Scripts/View/BoardAnimator.cs
@@ -24,59 +24,107 @@ public class BoardAnimator
 
     public IEnumerator AnimateMovements(List<Movement> movements)
     {
+        DisableInputHandler();
+
+        Transform canvasTransform = GetCanvasTransform();
+        Vector2 tileSize = GetTileSize();
+        List<Coroutine> animationCoroutines = StartTileAnimations(movements, canvasTransform, tileSize);
+
+        yield return WaitForCoroutinesToFinish(animationCoroutines);
+
+        FinalizeAnimation();
+    }
+
+    private void DisableInputHandler()
+    {
         inputHandler.enabled = false;
+    }
 
-        Transform canvasTransform = Object.FindAnyObjectByType<Canvas>().transform;
-        Vector2 tileSize = tileCreator.GetTileSize();
+    private Transform GetCanvasTransform()
+    {
+        return Object.FindAnyObjectByType<Canvas>().transform;
+    }
 
+    private Vector2 GetTileSize()
+    {
+        return tileCreator.GetTileSize();
+    }
+
+    private List<Coroutine> StartTileAnimations(List<Movement> movements, Transform canvasTransform, Vector2 tileSize)
+    {
         List<Coroutine> animationCoroutines = new List<Coroutine>();
 
         foreach (var movement in movements)
         {
-            Vector2Int startPos = movement.Start;
-            Vector2Int endPos = movement.End;
-            int value = board.GetTileValue(endPos);
-            Color originalColor = tileCreator.GetTileColor(startPos);
-
-            Vector3 startWorldPos = tileCreator.GetWorldPosition(startPos);
-            GameObject tileCopy = tileCreator.CreateTileInstance(startWorldPos, value, canvasTransform, tileSize, originalColor);
-
-            boardRenderer.SetTileValue(startPos, 0);
-            boardRenderer.SetTileColor(startPos, 0);
-
-            Coroutine animation = board.StartCoroutine(MoveTile(tileCopy, endPos, totalAnimationDuration, () =>
-            {
-                boardRenderer.SetTileValue(endPos, value);
-
-                if (movement.IsMerge)
-                {
-                    boardRenderer.SetTileColor(endPos, value);
-                }
-
-                Object.Destroy(tileCopy);
-            }));
+            Coroutine animation = StartTileAnimation(movement, canvasTransform, tileSize);
             animationCoroutines.Add(animation);
         }
 
-        foreach (var coroutine in animationCoroutines)
+        return animationCoroutines;
+    }
+
+    private Coroutine StartTileAnimation(Movement movement, Transform canvasTransform, Vector2 tileSize)
+    {
+        Vector2Int startPos = movement.Start;
+        Vector2Int endPos = movement.End;
+        int value = board.GetTileValue(endPos);
+        Color originalColor = tileCreator.GetTileColor(startPos);
+
+        Vector3 startWorldPos = tileCreator.GetWorldPosition(startPos);
+        GameObject tileCopy = tileCreator.CreateTileInstance(startWorldPos, value, canvasTransform, tileSize, originalColor);
+
+        ClearTileAtStartPosition(startPos);
+
+        return board.StartCoroutine(MoveTile(tileCopy, endPos, totalAnimationDuration, () =>
+        {
+            SetTileAtEndPosition(endPos, value, movement.IsMerge, tileCopy);
+        }));
+    }
+
+    private void ClearTileAtStartPosition(Vector2Int startPos)
+    {
+        boardRenderer.SetTileValue(startPos, 0);
+        boardRenderer.SetTileColor(startPos, 0);
+    }
+
+    private void SetTileAtEndPosition(Vector2Int endPos, int value, bool isMerge, GameObject tileCopy)
+    {
+        boardRenderer.SetTileValue(endPos, value);
+
+        if (isMerge)
+        {
+            boardRenderer.SetTileColor(endPos, value);
+        }
+
+        Object.Destroy(tileCopy);
+    }
+
+    private IEnumerator WaitForCoroutinesToFinish(List<Coroutine> coroutines)
+    {
+        foreach (var coroutine in coroutines)
         {
             yield return coroutine;
         }
+    }
 
+    private void FinalizeAnimation()
+    {
         boardRenderer.RenderBoard();
         score.UpdateScore();
+        EnableInputHandler();
+    }
 
+    private void EnableInputHandler()
+    {
         inputHandler.enabled = true;
     }
 
     private IEnumerator MoveTile(GameObject tile, Vector2Int endPos, float duration, System.Action onComplete)
     {
         Vector3 end = tileCreator.GetWorldPosition(endPos);
-
         Tween tween = tile.transform.DOMove(end, duration).SetEase(Ease.InOutCirc);
 
         yield return tween.WaitForCompletion();
-
         onComplete?.Invoke();
     }
 }
